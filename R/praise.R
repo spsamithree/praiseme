cefa605365d9f47fa526571dbb7fa455e87417d8
samithree <samithreer@gmail.com>
2017-10-25 15:56:50 +1100

    Initial commit

---
+++ R/praise.R
@@ -0,0 +1,13 @@
+#' @title Praise package
+#'
+#' @description My package for praise
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' # Here's my example
+praise <- function(){
+  "you're the best, Samithree!"
+}
